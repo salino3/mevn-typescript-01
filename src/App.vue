1ec071123a65b3660f1722dd8f2ef7991220a6c4
salino3 <flaviobcn012@gmail.com>
2023-07-14 12:20:38 +0200

    install tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
-<!-- npm i vue-router -->
+<!-- 
+  npm i vue-router
+  npm install -D tailwindcss postcss autoprefixer
+  npx tailwindcss init -p
+ -->
 
 <template>
  <main>
   <Navbar />
-  <h1>Hello World!</h1>
+  <h1 class="text-3xl font-bold underline bg-red-300 hover:bg-red-500 active:bg-blue-500">
+    Hello World!
+  </h1>
   <router-view />
  </main>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,vue,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
